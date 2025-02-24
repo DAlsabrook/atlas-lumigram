db50ca40f99a5196735663f24b8b29e98774fc88
David Alsabrook <Dfalsabrook@gmail.com>
2025-02-24 18:16:54 +0000

    dark mode for logout button

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,16 +1,20 @@
 import { Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function Logout() {
     const router = useRouter();
+    const colorScheme = useColorScheme();
+    const iconColor = colorScheme === 'dark' ? 'white' : 'black';
+
     function logout() {
-        router.replace("/login")
+        router.replace("/login");
     }
 
-    return(
+    return (
         <Pressable onPress={logout}>
-            <Ionicons name='log-out-outline' size={24} style={{ marginRight: 16}}></Ionicons>
+            <Ionicons name='log-out-outline' size={24} color={iconColor} style={{ marginRight: 16 }} />
         </Pressable>
-    )
+    );
 }
